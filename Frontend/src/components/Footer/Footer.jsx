218859840e5d22b4db3d2aa016df8c323e105bb9
Dhruv Garg <dhruv.g4445@gmail.com>
2024-10-14 00:58:03 +0530

    Update footer author name

--- Frontend/src/components/Footer/Footer.jsx
+++ Frontend/src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className=" bg-zinc-800 px-8 py-4 text-white">
-        <h1 className='text-xl font-semibold text-center'>&copy; 2024, Made with love by SAHIL</h1>
+        <h1 className='text-xl font-semibold text-center'>&copy; 2024, Made with love by DHRUV</h1>
     </div>
   )
 }
